0bf1d255c4db5958f2ef20d5ac6b61e3d09db67c
Sugar_F0x <malion.torchwood@yandex.ru>
2020-08-21 19:30:04 +0300

    :hammer_and_wrench: Not resolving API calls
    > API calls should return promise and then resolve/reject it

--- pages/api/dummy.js
+++ pages/api/dummy.js
@@ -1,6 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200;
-  res.json({ result: 1, message: 'Successful response' })
+export default async (req, res) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      res.statusCode = 200;
+      res.json({ result: 1, message: 'Successful response' });
+      resolve();
+    }, Math.floor(Math.random()*1000+250))
+  });
 }
